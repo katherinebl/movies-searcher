3710f47afd7aec125670a94ad435f16957d9a79a
katherinebl <kath.gbl@gmail.com>
2020-05-26 19:17:18 +0200

    Added 2 methods and state to the component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react'
 
 export class SearchForm extends Component {
+  state =  {
+    inputMovie: ''
+  }
+
+  _handleChange = (e) => {
+    this.setState({ inputMovie: e.target.value })
+  }
+
+  _handleSubmit = (e) => {
+    e.preventDefault();
+    alert(this.state.inputMovie);
+  }
+
   render() {
     return (
-      <div class="field has-addons">
-        <div class="control">
-          <input 
-            class="input" 
-            type="text" 
-            placeholder="Find a repository"
-          />
+      <form onSubmit={this._handleSubmit}>
+        <div class="field has-addons">
+          <div class="control">
+            <input 
+              class="input" 
+              type="text" 
+              placeholder="Search for a movie..."
+              onChange={this._handleChange}
+            />
+          </div>
+          <div class="control">
+            <a class="button is-info">
+              Search
+            </a>
+          </div>
         </div>
-        <div class="control">
-          <a class="button is-info">
-            Search
-          </a>
-        </div>
-      </div>
+      </form>
+      
     )
   }
 }
